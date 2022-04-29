4ae5ec90309029acdb565c20713db7a7585cd918
Pankaj Kumar <pankaj3359kr@gmail.com>
2022-04-29 23:51:47 +0530

    Update project

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,35 +2,43 @@ const projects = [
     
     {
         id : '0',
+        title : "Expense Tracker",
+        language : 'JavaScript',
+        description: 'This app keeps track of the expenses and helps the user manage their budget.',
+        tags : ['React', 'Bootstrap'],
+        url: 'https://github.com/iPankajKr/expense-tracker#readme'
+    },
+    {
+        id : '1',
         title : "Tesla Clone",
         language : 'JavaScript',
         description: 'Clone of Tesla website landing page built using React.',
         tags : ['React', 'Redux', 'Styled Components'],
-        url: 'https://github.com/iPankajKr/tesla-clone'
+        url: 'https://github.com/iPankajKr/tesla-clone#readme'
     },
     {
-        id: '1',
+        id: '2',
         title: "Meme Generator",
         language: 'JavaScript',
         description: 'Meme genrator app that fetches a random meme image from a remote API and adds your custom text over it.',
         tags: ['React'],
-        url: 'https://github.com/iPankajKr/meme-generator'
+        url: 'https://github.com/iPankajKr/meme-generator#readme'
     },
     {
-        id : '2',
+        id : '3',
         title: "BMI Calculator",
         language: 'JavaScript',
         description: 'Simple BMI Calculator app built using React.',
         tags: ['React', 'Tailwind CSS'],
-        url: 'https://bmi-calculator-pankaj.vercel.app/'
+        url: 'https://github.com/iPankajKr/bmi-calculator'
     },
     {
-        id: '3',
+        id: '4',
         title: "Simple Portfolio Website",
         language: 'HTML',
         description: 'Portfolio website built using HTML, CSS, and JavaScript',
         tags: ['HTML', 'CSS', 'JavaScript'],
-        url: 'https://ipankajkr.github.io/portfolio/'
+        url: 'https://ipankajkr.github.io/portfolio/#readme'
     }
 ]
 
